097736b6095b6f341330683723416370f38eb9a4
hanhhle <hoanganhle03201208@gmail.com>
2026-04-03 13:27:26 +0700

    Add patient management header file

---
+++ patient_management.h
@@ -0,0 +1,37 @@
+#ifndef PATIENT_MANAGEMENT_H
+#define PATIENT_MANAGEMENT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+typedef struct {
+    char id[10];
+    char name[50];
+    int age;
+    char gender[10];
+    char disease[100];
+    char admissionDate[11];
+} Patient;
+
+typedef struct Node {
+    Patient data;
+    struct Node* next;
+} Node;
+
+Node* createNode(Patient p);
+void addPatient(Node **head, Patient p);
+int isDuplicate(Node *head, char id[]);
+Node* searchPatient(Node *head, char id[]);
+void updatePatient(Node *head, char id[]);
+void deletePatient(Node **head, char id[]);
+
+void loadFromFile(Node **head);
+void saveToFile(Node *head);
+void freeList(Node *head);
+
+void displayPatients(Node *head);
+void mergeSort(Node **headRef);
+
+#endif // PATIENT_MANAGEMENT_H
