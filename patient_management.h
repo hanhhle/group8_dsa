fb53bb8ff929e60583628ec566057fe50cfef7b8
hanhhle <hoanganhle03201208@gmail.com>
2026-05-18 14:24:09 +0700

    fix_stucture

--- patient_management.h
+++ patient_management.h
@@ -2,40 +2,62 @@
 #define PATIENT_MANAGEMENT_H
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
+#include <stdlib.h> 
 
-typedef struct {
+struct Doctor {
+    char doctorId[10];          // Doctor ID
+    char name[50];              // Doctor name
+    char specialization[50];    // Medical specialization
+};
+
+struct Medicine {
+    char medicineId[10];        // Medicine ID
+    char medicineName[50];      // Medicine name
+    double price;               // Medicine price
+};
+
+
+struct Treatment {
+    char diagnosis[100];            // Detailed diagnosis
+    char method[150];               // Treatment method
+    char prescribedMedicineId[10];  // Prescribed medicine ID
+};
+
+struct VisitRecord {
+    char arrivalDate[11];
+    char symptoms[100];
+    struct Doctor assignedDoctor;
+    struct Treatment currentTreatment;
+};
+
+struct Patient {
     char id[10];
     char name[50];
     int age;
     char gender[10];
-    char disease[100];
-    char admissionDate[11];
-} Patient;
+    struct VisitRecord* history;
+    int visitCount;
+    int capacity;
+};
 
 typedef struct Node {
-    Patient data;
+    struct Patient data;
     struct Node* next;
 } Node;
 
-int readLine(char *buffer, size_t size);
-int readInt(const char *prompt, int *outValue);
-int isValidDate(const char *date);
-
-Node* createNode(Patient p);
-void addPatient(Node **head, Patient p);
-int isDuplicate(Node *head, char id[]);
+// 1. PATIENT MANAGEMENT MODULE
+Node* createNode(struct Patient p);
+void addPatient(Node **head, struct Patient p);
 Node* searchPatient(Node *head, char id[]);
-void updatePatient(Node *head, char id[]);
 void deletePatient(Node **head, char id[]);
-
-void loadFromFile(Node **head);
+void displayPatients(Node *head);
+void updatePatient(Node *head, char id[]);
 void saveToFile(Node *head);
+void loadFromFile(Node **head);
 void freeList(Node *head);
 
-void displayAllPatients(Node *head);
-void mergeSort(Node **headRef);
-
-#endif // PATIENT_MANAGEMENT_H
+// 2. VISIT HISTORY MANAGEMENT MODULE
+void addVisitRecord(struct Patient *p,
+                    struct VisitRecord newVisit);
+void displayVisitHistory(struct Patient *p);
+void expandVisitHistory(struct Patient *p);
